2525678ef267e9660b5569aa06d4a131995f8a5a
stoguri11 <samtoguri@outlook.com>
2021-02-20 11:27:13 +0000

    Adding unit content

--- front/src/index.js
+++ front/src/index.js
@@ -1,14 +1,19 @@
 $(() => {
 
     $('button.dropdown-toggle').on("click", function (e) {
-        $(this).closest('div').toggleClass('open');
-        e.stopPropagation();
         e.preventDefault();
+        $(this).closest('div').toggleClass('open');
     });
 
-    $("#menu-toggle").on("click", (e) => {
+    $("#menu-toggle, .dropitem").on("click", (e) => {
         e.preventDefault();
         $("#wrapper").toggleClass("toggled");
+    });
+
+    $("#content-wrapper").on("click", (e) => {
+        e.preventDefault();
+        console.log('click');
+        $("#wrapper").addClass("toggled");
     });
 
     $(".dropitem").click(function (e) {
@@ -16,31 +21,52 @@ $(() => {
 
         var clickText = $(e.target).text();
 
-        $("currentPage").text(() => {
-            return clickText ;
+        $("#currentPage").text(() => {
+            return clickText;
         });
 
         let content;
-        switch (clickText) {
+        if (clickText == "Masters Project") {
+            content = "./html/y4/mastproj.html";
+        } else if (clickText == "Applied Data and Text Analytics") {
+            content = "./html/y4/adta.html";
+        } else if (clickText = "Parallel Programming") {
+            content = "./html/y4/pp.html";
+        } else if (clickText = "Computer Vision") {
+            content = "./html/y4/comvi.html";
+        } else if (clickText = "Scientific Computing and Simulation") {
+            content = "./html/y4/scas.html"
+        } else if (clickText = "Final Year Project") {
+            content = "./html/y3/fyp.html";
+        } else if (clickText = "Theoretical Comuter Science") {
+            content = "./html/y3/theocs.html";
+        } else if (clickText = "Distributed Systems") {
+            content = "./html/y3/disys.html";
+        } else if (clickText = "Robotics") {
+            content = "./html/y3/rob.html";
+        } else if (clickText = "Enterprise Web Architectures") {
+            content = "./html/y3/entwa.html";
+        } else if (clickText = "Data and Algorithms") {
+            content = "./html/y2/dsalg.html";
+        } else if ("Maths and Functional Programming") {
+            content = "./html/y2/mafp.html";
+        } else if (clickText = "Operating Systems and Networking") {
+            content = "./html/y2/osan.html";
+        } else if (clickText = "Advanced Programming Concepts") {
+            content = "./html/y2/adproc.html";
+        } else if (clickText = "Software Engineering") {
+            content = "./html/y2/inse.html";
+        } else if (clickText = "Web Programming") {
+            content = "./html/y2/webprog.html";
+        } else if (clickText = "Year 1") {
+            content = "./html/year1.html";
+        } else (
+            content = ",./html/about.html"
+        )
 
-            case "Web Foundations":
-                content = "./webf.html";
-                break;
-
-            case "Introduction to Programming":
-                content = "./intprog.html";
-                break;
-
-            case "Network Fundamentals":
-                content = "./netfun.html";
-                break;
-
-            case "About me":
-                content = "./about.html";
-                break;
-        }
-
-        $("#contentarea").load(content);
+            $.get(content, async (html) => {
+                $("#contentarea").html(html);
+            });
     });
 
 });
--- server/App.js
+++ server/App.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const index = require('./routes/Index');
 
@@ -6,8 +7,11 @@ const index = require('./routes/Index');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(__dirname + "../../front/src/html"));
 app.use(express.static(__dirname + "../../front/src/"));
+app.use(express.static(__dirname + "../../front/src/html"));
+app.use(express.static(__dirname + "../../front/src/html/y2"));
+app.use(express.static(__dirname + "../../front/src/html/y3"));
+app.use(express.static(__dirname + "../../front/src/html/y4"));
 
 // For parsing application/json 
 app.use(express.json()); 
@@ -15,6 +19,10 @@ app.use(express.json());
 // For parsing application/x-www-form-urlencoded 
 app.use(express.urlencoded({ extended: true })); 
 
+// logging
+
+
+// serve index page
 app.use("/", index);
 
 app.listen(PORT, () => {
