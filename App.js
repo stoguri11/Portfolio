c01a56f2cd44e24fa34406ebb35dc0ed723f2c8b
stoguri11 <samtoguri@outlook.com>
2021-03-29 18:34:34 +0100

    api updates

--- App.js
+++ App.js
@@ -6,10 +6,10 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '/frontend/build')));
+app.use(express.static(path.join(__dirname, '/frontend/build/')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/frontend/build', 'index.html'));
+    res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
   });
 
 // access logging
